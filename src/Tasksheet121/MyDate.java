bace25e74506a94fa5138527bfaa1ef2f82c1a14
Lemuel825 <lem25.tresvalles@gmail.com>
2024-06-28 10:55:21 +0800

    june 28

---
+++ src/Tasksheet121/MyDate.java
@@ -0,0 +1,15 @@
+package Tasksheet121;
+import Tasksheet121.DateTask;
+
+import javax.xml.crypto.Data;
+
+public class MyDate {
+    public static void main(String[] args) {
+        DateTask date1 = new DateTask(1,1,1978);
+        DateTask date2 = new DateTask(9,21,1984);
+        System.out.println(date1.toString());
+        System.out.println(date2.toString());
+        DateTask.leapYears();  // display leapyears
+
+    }
+}
