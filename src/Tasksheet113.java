dee94757a1723449b9f0b88b51171750e79dbb81
Lemuel825 <lem25.tresvalles@gmail.com>
2024-06-28 09:43:12 +0800

    june 28

---
+++ src/Tasksheet113.java
@@ -0,0 +1,10 @@
+public class Tasksheet113 {
+    public static void main(String[] args) {
+        int check_number = 10;
+        String message;
+
+        for(int i = 1; i <= check_number; i++){
+            System.out.println(message = (i%2==0) ? (i+ " is even number" ):(i+ " is odd number"));
+        }
+    }
+}
